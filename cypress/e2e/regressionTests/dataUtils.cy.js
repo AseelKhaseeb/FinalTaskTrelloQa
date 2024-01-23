e2efe6a467ea4a93afe0af6a5b057e5b97e03012
AseelKh <aseelkh.work@gmail.com>
2024-01-23 23:23:19 +0200

    UpdateListName

---
+++ cypress/e2e/regressionTests/dataUtils.cy.js
@@ -0,0 +1,35 @@
+import {APIKEY, APITOKEN} from '../../support/constants.cy'
+
+class SharedDataUtils {
+    createNewBoard = (boardName)=>{
+        return cy.request("POST",`https://api.trello.com/1/boards/?name=${boardName}&key=${APIKEY}&token=${APITOKEN}`)
+    }
+
+    deleteBoard = (boardId)=>{
+        return cy.request("DELETE",`https://api.trello.com/1/boards/${boardId}?key=${APIKEY}&token=${APITOKEN}`)
+    }
+
+    getListsOfBoard = (boardId)=>{
+        return cy.request(`https://api.trello.com/1/boards/${boardId}/lists?key=${APIKEY}&token=${APITOKEN}`,{header: "Accept: application/json"})
+    }
+
+    createNewCard = (listId,cardName)=>{
+        return cy.request({
+            method: "POST",
+            url: `https://api.trello.com/1/cards?idList=${listId}&key=${APIKEY}&token=${APITOKEN}`,
+            header: "Accept: application/json",
+            body: {name: cardName}
+        })
+    }
+
+    createTemplate = (listId,templateCardName)=>{
+        return cy.request({
+            method: "POST",
+            url: `https://api.trello.com/1/cards?idList=${listId}&key=${APIKEY}&token=${APITOKEN}`,
+            header: "Accept: application/json",
+            body: {name: templateCardName, isTemplate: true}
+        })
+    }
+}
+
+export default SharedDataUtils
