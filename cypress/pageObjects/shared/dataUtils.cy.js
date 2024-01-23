51fd608f3ecb4462d5aa31b56e17c03d6ae6a94c
AseelKh <aseelkh.work@gmail.com>
2024-01-23 23:25:07 +0200

    DeleteCard

--- cypress/pageObjects/shared/dataUtils.cy.js
+++ cypress/pageObjects/shared/dataUtils.cy.js
@@ -4,6 +4,10 @@ class SharedDataUtils {
     createNewBoard = (boardName)=>{
         return cy.request("POST",`https://api.trello.com/1/boards/?name=${boardName}&key=${APIKEY}&token=${APITOKEN}`)
     }
+
+    deleteBoard = (boardId)=>{
+        return cy.request("DELETE",`https://api.trello.com/1/boards/${boardId}?key=${APIKEY}&token=${APITOKEN}`)
+    }
 }
 
 export default SharedDataUtils
